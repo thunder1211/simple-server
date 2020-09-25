d9f52ba2cadcfad597f0d92363dd51c08063a56d
joseph <joseph1211@126.com>
2020-09-25 14:03:52 +0800

    添加判断,端口是否被占用

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var util = require('util');
 var fs = require('fs');
 var path = require("path");
 var router = require('./fileRoute');
-var port = process.argv[2] || 8080;
+var probe = require('./probe')
 var pagePath = "/index.html"
 
 
@@ -14,11 +14,20 @@ router.get('/', function(req, res){
     // 文件首页
     router.sendFile(res, pagePath);
 });
-
-
-const srv = http.createServer((req, res) => {
-    router.init(req, res);
+console.log(process.argv)
+var port = process.argv[2] || 8080;
+probe(port, function(enabled, _pt){
+    // 端口被占用 enabled 返回false
+    // _pt：传入的端口号
+    if(enabled !== true) {
+        _pt += 10
+    }
+    const srv = http.createServer((req, res) => {
+        router.init(req, res);
+    })
+    
+    // 监听端口
+    srv.listen(_pt);
+    console.log('connect to : http://localhost:'+ _pt);
+    
 })
-
-// 监听端口
-srv.listen(port);
